f95a2a6d91fd9f21eda54c4cdc6ee468f4e68ae8
DFong298 <dennis1.fong@gmail.com>
2022-10-01 22:16:13 -0400

    Tests for pathfinding algorithms

--- graphlib/metrics/paths/dijkstra.py
+++ graphlib/metrics/paths/dijkstra.py
@@ -4,6 +4,6 @@ from graphlib.metrics.paths.astar import AStarPathMetric
 class DijkstraShortestPathMetric(AStarPathMetric):
     '''A specialization of A* with no heuristic function'''
 
-    def __call__(self, fr, to, weight_func):
+    def __call__(self, fr, to, weight_func=None):
         self.set_heuristic_func(lambda u: 0)
         return super().__call__(fr, to, weight_func)
---
+++ tests/test_pathfind.py
@@ -0,0 +1,56 @@
+import pytest
+import os
+import sys
+FILE_PATH = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(FILE_PATH, ".."))
+
+from graphlib.metrics import TubemapItinerary  # noqa: E402
+from graphlib.builders import TubemapCSVBuilder  # noqa: E402
+from graphlib.metrics.paths import DijkstraShortestPathMetric  # noqa: E402
+from graphlib.metrics.paths import AStarPathMetric  # noqa: E402
+
+
+@pytest.fixture
+def short_path():
+    return (197, 250), [197, 151, 60, 126, 48, 250]
+
+
+@pytest.fixture
+def long_path():
+    return (110, 247), [110, 17, 74, 99, 236, 229, 273, 248, 285, 279, 13, 156,
+                        24, 164, 247]
+
+
+@pytest.fixture
+def all_cases(short_path, long_path):
+    """Using fixture composition to bring all the cases together"""
+    return [short_path, long_path]
+
+
+def is_correct_path(output, expected_path):
+    actual_path = list(map(lambda e: e.fr, output)) + [output[-1].to]
+    return len(actual_path) == len(expected_path) and \
+        all(i == j for i, j in zip(actual_path, expected_path))
+
+
+def do_test(algo_name, source, destination):
+    tubemap_builder = TubemapCSVBuilder(
+        os.path.join(FILE_PATH, "../_dataset/london.stations.csv"),
+        os.path.join(FILE_PATH, "../_dataset/london.connections.csv"),
+        os.path.join(FILE_PATH, "../_dataset/london.lines.csv")
+    )
+    tubemap_graph = tubemap_builder.build()
+    itinerary = TubemapItinerary(source, destination)
+    if algo_name == "Dijkstra":
+        itinerary.set_search_strategy(
+            DijkstraShortestPathMetric(tubemap_graph))
+    elif algo_name == "AStar":
+        itinerary.set_search_strategy(AStarPathMetric(tubemap_graph))
+    return itinerary.shortest_path()
+
+
+@pytest.mark.parametrize("metric_name", ["Dijkstra", "AStar"])
+def test_pathfinding(metric_name, all_cases):
+    for case, expected in all_cases:
+        results = do_test(metric_name, *case)
+        assert is_correct_path(results, expected)
